ba7f432c19d8e42bcc255295dd4736ebddab5410
dawid-pawlowski <dawid.pawlowski@zielonykod.com>
2023-09-08 12:50:12 +0200

    polishing feeding admin

--- babies/admin.py
+++ babies/admin.py
@@ -10,12 +10,10 @@ TWO_HOURS_IN_S = 60 * 60 * 2
 
 class FeedingAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
-        # For now I assume that we are adding same day feedings as I will change field to be datetime anyway with postgres
         t = timezone.now() - obj.created_at
+        countdown = TWO_HOURS_IN_S - t.total_seconds()
         if t.total_seconds() > TWO_HOURS_IN_S:
-            countdown = 1
-        else:
-            countdown = TWO_HOURS_IN_S - t.total_seconds()
+            countdown = None            
         send_feeding_mail(
             mail_subject="Feeding time!",
             target_mail=obj.created_by.email,
